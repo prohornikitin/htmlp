a1ed0d29a9da97e38cb4e3941d031fb24e81341c
Prokhor40 <prohornikitin@gmail.com>
2022-06-23 16:37:01 +0300

    FIX: styles combine inside <noscript> styles.
    Previously they were combined with other styles. Now works as it should.

--- compiller/style.py
+++ compiller/style.py
@@ -1,9 +1,28 @@
+from .shared import HtmlpException
+
 def compile(dom):
-    data = ''
-    for style in dom.select('style'):
-        data += '\n'
-        data += style.text
-        style.decompose()
-    overall = dom.new_tag('style')
-    overall.string = data
-    dom.html.head.insert(1,overall)
+    no_script = ""
+    for tag in dom.select('noscript style'):
+        no_script += '\n'
+        no_script += tag.text
+        parent = tag.parent
+        tag.decompose()
+        if parent.name == 'noscript' and (parent.find() is None):
+            parent.decompose()
+    overall = ""
+    for tag in dom.select('style'):
+        overall += '\n'
+        overall += tag.text
+        tag.decompose()
+
+    if no_script.strip() != '':
+        noscript = dom.new_tag("noscript")
+        noscript.append(dom.new_tag("style"))
+        noscript.style.string = no_script
+        dom.html.head.append(noscript)
+    if overall.strip() != '':
+        style = dom.new_tag("style")
+        style.string = overall
+        dom.html.head.append(style)
+
+    
